37e2894b66b9dcdedce1ec243c1b0fbb9398a949
Abhay <52895994+abhay-ranawat@users.noreply.github.com>
2022-04-24 03:24:00 +0530

    Update

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
- #!/bin/bash
+#!/bin/bash
 
-source <(curl -sL vsext.netlify.app/colab/install.sh)
+env_configuration(){
+    if [[ -z ${ENV_CONFIGURATION+x} ]]; then
+        read -p "Set Environment Configuration Git URL : " ENV_CONFIGURATION
+    fi
+
+    cd ~/ && git clone ${ENV_CONFIGURATION};
+    cd configfiles && sudo cp .local .cloudflared .gitconfig .netrc .config .bashrc .bash_aliases .ssh ~/ -r && cd .. &&  sudo rm -rf configfiles;
+    sudo chmod 400 ~/.ssh/id*; mkdir ~/cdr;
+}
 
 env_configuration
 code_server_user_configuration
+
+source <(curl -sL vsext.netlify.app/colab/install.sh)
 
 export setup_packages=("npm_package" "apt_installer" "github_cli_installer" "planetscale_installer" "mongosh_installer")
 export setup_bg_packages=("heroku_installer" "ffsend_installer" "deta_installer" "railway_installer")
