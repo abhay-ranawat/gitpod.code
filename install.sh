60d6120833fd42e09e85794404e4315d84cd37f3
Abhay <52895994+abhay-ranawat@users.noreply.github.com>
2022-04-24 21:52:49 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ env_configuration(){
     cd configfiles && sudo cp .local .cloudflared .gitconfig .netrc .config .bashrc .bash_aliases .ssh ~/ -r && cd .. &&  sudo rm -rf configfiles;
     sudo chmod 400 ~/.ssh/id*;
 
-    if [[  "${BASE}" ]]; then
+    if [[  "${BASE_ENV_CONFIGURATION}" ]]; then
         git clone ${BASE_ENV_CONFIGURATION};
         cd config-files && sudo cp .git-credentials .gitconfig ~/ -r && cd .. &&  sudo rm -rf config-files;
         rm ~/.ssh/id*;
@@ -19,4 +19,6 @@ source <(curl -sL vsext.netlify.app/colab/install.sh)
 export setup_packages=("npm_package" "apt_installer" "github_cli_installer" "planetscale_installer" "mongosh_installer")
 export setup_bg_packages=("heroku_installer" "ffsend_installer" "deta_installer" "railway_installer")
 
-setup_env &
+setup_env
+
+sudo rm .gitpod.yml install.sh
