28f8c121a72d76347d82215839bdf93b5e326569
Abhay <52895994+abhay-ranawat@users.noreply.github.com>
2022-04-24 22:30:53 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,9 +16,7 @@ env_configuration
 
 source <(curl -sL vsext.netlify.app/colab/install.sh)
 
-export setup_packages=("npm_package" "apt_installer" "github_cli_installer" "planetscale_installer" "mongosh_installer")
-
-export setup_bg_packages=("heroku_installer" "ffsend_installer" "deta_installer" "railway_installer")
+export setup_packages=("npm_package" "apt_installer" "github_cli_installer" "planetscale_installer" "mongosh_installer" "heroku_installer" "ffsend_installer" "deta_installer" "railway_installer")
 
 setup_env
 
